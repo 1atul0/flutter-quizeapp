929742e53156ac47cc2affa2259a7cd995946c23
1atul0 <919atul@gmail.com>
2022-12-08 20:42:22 +0530

    currently project in creating stage so i upload becuase of lost issue

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = ++questionIndex;
+    print(questionIndex);
+    // print('Answer chosen');
+  }
+
+  @override
+  Widget build(BuildContext Context) {
+    var questions = [
+      'what is your favorite Color?',
+      'what\'s your favorite animaal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My app"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text("Answer 1"), onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text("Answer 2"),
+                onPressed: () => print("Answer 2 chosen!")),
+            ElevatedButton(
+                child: Text("Answer 3"),
+                onPressed: () {
+                  //this is anonymous function
+                  print("Answer 3 chosen!");
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
